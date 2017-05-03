2224caab126feb1dff3f9a1dc642a66b79a4874c
Roger Melich <rogermelich@gmail.com>
2017-05-03 19:34:56 +0200

    Finally Firts version

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,14 @@
-window.load = function () {
+window.onload = function () {
 
     var states = {
         preload: preload,
         create: create
     }
     
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '#game', states);
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', states);
     
     function preload() {
-        game.load.image('logo', 'assets/Phaser.png')
+        game.load.image('logo', '../assets/Phaser.png')
     }
 
     function create() {
